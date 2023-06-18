741d2927847b55808e90caa69dce9414f0a5757d
ledmonds <leuan.edmonds@gmail.com>
2023-06-18 09:53:03 +0200

    chore: Adding a command-line argument parser to assist in calling the application.
    This commit adds the ability for the application to accept a case-insensitive `ActionArgument` (`-a [ACTION]` or `--action ACTION]`). The current options are:
    - `startup`: Adds a startup entry to the application.
    - `shutdown`: Adds a shutdown entry to the application.
    - `present`: Outputs a projection of the currently recorded data.

--- src/startup-shutdown-tracker.Presentation/Program.cs
+++ src/startup-shutdown-tracker.Presentation/Program.cs
@@ -1,14 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Fclp;
 using Microsoft.Extensions.DependencyInjection;
 using startup_shutdown_tracker.Application;
-using System;
+using System.Linq;
+using System.Threading.Tasks;
 
-var serviceProvider = new ServiceCollection().InjectDependencies().BuildServiceProvider();
+internal class Program
+{
+	private static async Task Main(string[] args)
+	{
+		var serviceProvider = new ServiceCollection().InjectDependencies().BuildServiceProvider();
 
-var trackerService = serviceProvider.GetRequiredService<TrackerService>();
+		var applicaitonArguments = new FluentCommandLineParser<ApplicationArguments>();
 
-await trackerService.AddStartupEntry();
-await trackerService.AddShutdownEntry();
+		applicaitonArguments
+			.Setup(x => x.Action)
+			.As('a', "action")
+			.SetDefault(ApplicationAction.Present);
 
-Console.WriteLine("testing");
+		applicaitonArguments.Parse(args.ToArray());
+
+		var trackerService = serviceProvider.GetRequiredService<TrackerService>();
+
+		switch (applicaitonArguments.Object.Action)
+		{
+			case ApplicationAction.Startup:
+				await trackerService.AddStartupEntry();
+				break;
+
+			case ApplicationAction.Shutdown:
+				await trackerService.AddShutdownEntry();
+				break;
+
+			case ApplicationAction.Present:
+				break;
+		}
+	}
+}
